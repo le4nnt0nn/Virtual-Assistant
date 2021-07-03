e440a8d035ae5a06ba5c6e643478502cd76468d9
Antonio <ditacj78@gmail.com>
2021-07-03 18:17:47 +0200

    mizu.py : created method listen and run

--- mizu.py
+++ mizu.py
@@ -2,7 +2,7 @@ import speech_recognition as  sr
 import pyttsx3
 
 # - puedes cambiar el nombre al que quieras
-name = "ruka"
+name = "cuca"
 listener = sr.Recognizer()
 
 engine = pyttsx3.init()
@@ -12,16 +12,30 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+def listen():
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            voice = listener.listen(source)
+            rec = listener.recognize_google(voice)
+            rec = rec.lower()
+            # - si el nombre seleccionado se escucha, lo repite
+            if name in rec:
+                rec = rec.replace(name, '')
+                print(rec)
+            # talk(rec) - repite lo que dices    
+            # print(rec) - imprime lo que dices
+    except:
+        pass
+    # - retorna rec
+    return rec
 
-try:
-    with sr.Microphone() as source:
-        print("Escuchando...")
-        voice = listener.listen(source)
-        rec = listener.recognize_google(voice)
-        rec = rec.lower()
-        # - si el nombre seleccionado se escucha, lo repite
-        if name in rec:
-            talk(rec)
-        # print(rec) - imprime lo que dices
-except:
-    pass
+def run():
+    # - rec recoge el valor de listen
+    rec = listen()
+    if 'reproduce' in rec:
+        music = rec.replace("reproduce", "")
+        talk("Reproduciendo "+ music)
+
+
+run()
