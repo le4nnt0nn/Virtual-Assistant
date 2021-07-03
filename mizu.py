0ab763578e57c977ea72c1f8fb3339123d0539c1
Antonio <ditacj78@gmail.com>
2021-07-03 17:58:57 +0200

    mizu.py : added talk method and import pyttsx3

--- mizu.py
+++ mizu.py
@@ -1,6 +1,14 @@
 import speech_recognition as  sr
+import pyttsx3
 
 listener = sr.Recognizer()
+
+engine = pyttsx3.init()
+
+# - este método permite que la máquina repita lo que tu digas
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 
 
 try:
@@ -8,6 +16,7 @@ try:
         print("Escuchando...")
         voice = listener.listen(source)
         rec = listener.recognize_google(voice)
-        print(rec)
+        talk(rec)
+        # print(rec) - imprime lo que dices
 except:
     pass
