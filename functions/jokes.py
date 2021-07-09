c42a34fd3fd1918fc7378c609cf9c0dee91e05d0
Antonio <ditacj78@gmail.com>
2021-07-09 11:38:02 +0200

    functions : created jokes

---
+++ functions/jokes.py
@@ -0,0 +1,11 @@
+import talk
+
+def joke(rand):
+    if rand==0:
+        talk("Van dos cieguitos por la calle pasando calor y dicen: ¡Ojalá lloviera! ¡Ojalá yo también!")
+    elif rand==1:
+        talk("Qué le dice una iguana a su hermana gemela? Somos iguanitas")
+    elif rand==2:
+        talk("Qué hace un mudo bailando? Una mudanza")
+    elif rand==3:
+        talk("Rápido necesitamos sangre. Yo soy 0 positivo. Muy mal, aqui se viene a animar")
