1f2eb9e0e91ac0b8bc44db7d5d66f9869cc0469c
Antonio <ditacj78@gmail.com>
2021-07-11 00:22:54 +0200

    calendar.py : added comments

--- functions/calendar.py
+++ functions/calendar.py
@@ -2,5 +2,7 @@ import calendar
 
 def goCalendar():
     c = calendar.TextCalendar(calendar.SUNDAY)
+    # - el calendario empieza en domingo
     str = c.formatmonth(2021, 7)
+    # - el calendario pertenecerá al año 2021 y al mes de julio. Se puede cambiar al que prefieras
     print(str)
