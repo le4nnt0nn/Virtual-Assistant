fdb8481336ce8d85aa89827018eb304f74243f33
Antonio <ditacj78@gmail.com>
2021-07-12 13:07:41 +0200

    calendar.py : added currentYear and currentMonth

--- functions/calendar.py
+++ functions/calendar.py
@@ -1,8 +1,11 @@
 import calendar
+from datetime import datetime
 
 def goCalendar():
+    currentMonth = datetime.now().month
+    currentYear = datetime.now().year
     c = calendar.TextCalendar(calendar.SUNDAY)
     # - el calendario empieza en domingo
-    str = c.formatmonth(2021, 7)
+    str = c.formatmonth(currentYear, currentMonth)
     # - el calendario pertenecerá al año 2021 y al mes de julio. Se puede cambiar al que prefieras
     print(str)
